3954996518b9ccc9a622c1a7af4a24beb833e9d4
overthaught-lgtm <overthaught@gmail.com>
2026-07-06 19:16:37 -0600

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,13 +1,9 @@
-import os
 from flask import Flask, render_template
 
-app = Flask(__name__, template_folder="../templates", static_folder="../static")
+# 1. Must be named exactly 'app' (lowercase)
+app = Flask(__name__, template_folder='../templates') 
 
-
-@app.route("/")
-def home() -> str:
-    return render_template("index.html")
-
-
-if __name__ == "__main__":
-    app.run(debug=True)
+@app.route('/')
+def home():
+    # 2. Make sure it points to your index.html file
+    return render_template('index.html') 
