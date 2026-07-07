4bd7777a4b8776d44e7a9c28734f9f7592373299
overthaught-lgtm <overthaught@gmail.com>
2026-07-06 18:09:07 -0600

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,11 +1,13 @@
-from flask import Flask, render_template
 import os
+from flask import Flask, render_template
 
-app = Flask(__name__, template_folder='../templates', static_folder='../static')
+app = Flask(__name__, template_folder="../templates", static_folder="../static")
 
-@app.route('/')
-def home():
-    return render_template('index.html')
 
-if __name__ == '__main__':
+@app.route("/")
+def home() -> str:
+    return render_template("index.html")
+
+
+if __name__ == "__main__":
     app.run(debug=True)
